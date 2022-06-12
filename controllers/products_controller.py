fb5202b686bf49c1311d9b9eb4a42e2860d068ed
Calum Cook <calumcook@Calums-Air.broadband>
2022-06-12 19:38:06 +0100

    info added to console and htmls

--- controllers/products_controller.py
+++ controllers/products_controller.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask import Blueprint
-from repositories import product_repository
+from models.product import Product
+import repositories.product_repository as product_repository
+import repositories.supplier_repository as supplier_repository
 
 product_repository = Blueprint("products", __name__)
